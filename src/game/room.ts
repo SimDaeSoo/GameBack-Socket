53fce9661f7841a65dd8f6369bf21583ea724c10
SimDaeSoo <tlaeotn123@naver.com>
2019-09-09 23:10:41 +0900

    Room 최대 명수 제한 변경.

--- src/game/room.ts
+++ src/game/room.ts
@@ -25,7 +25,7 @@ export class Room {
             name: '',
             members: [],
             isPlaying: false,
-            maxMembers: 1
+            maxMembers: 10
         }, options);
 
         for (let key in defaultOptions) {
